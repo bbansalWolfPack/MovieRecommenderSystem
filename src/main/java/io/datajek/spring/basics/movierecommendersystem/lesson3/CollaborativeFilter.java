774b97550abf4146914aee67ac9f2d4cbb27210d
Bhavya Bansal <bhbansal@amazon.com>
2022-12-22 10:07:12 -0800

    adding basic spring beans and autowiring support

---
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/lesson3/CollaborativeFilter.java
@@ -0,0 +1,10 @@
+package io.datajek.spring.basics.movierecommendersystem.lesson3;
+
+
+
+public class CollaborativeFilter implements Filter {
+    public String[] getRecommendations(String movie) {
+        //logic of collaborative based filter
+        return new String[] {};
+    }
+}
